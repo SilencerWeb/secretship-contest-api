8869efdb47e0f65b49fdec5deefadfe27f057464
Maksim Gorodov <silencer.web@gmail.com>
2019-06-09 10:45:30 +0500

    Set up database; Enable CORS; Add dependency cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 const app = express();
-const port = 3000;
+app.use(cors());
 
-app.listen(port, () => console.log(`API is up and running on port ${port}`));
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+} else {
+  mongoose.connect('mongodb://localhost:27017/secretship-contest', { useNewUrlParser: true });
+  mongoose.set('debug', true);
+}
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
