d63b1c4966d6f9ad6011e9a66b9c95817083888b
Maksim Gorodov <silencer.web@gmail.com>
2019-06-09 12:13:00 +0500

    Update model's User schema

--- src/models/user.js
+++ src/models/user.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-  id: String,
-  first_name: String,
+  id: { type: String, unique: true, required: true },
+  first_name: { type: String, required: true },
   last_name: String,
   username: String,
   avatarUrl: String,
-  created: Date,
+  created: { type: Date, default: Date.now() },
 });
 
 mongoose.model('User', userSchema);
